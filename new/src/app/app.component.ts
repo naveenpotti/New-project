f6d2a717c100a0c94efbea72178e4af0d9e6b4ff
pradeepkodumuri1 <naveen.w20@gmail.com>
2018-03-15 01:12:41 -0500

    search functionality

--- new/src/app/app.component.ts
+++ new/src/app/app.component.ts
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  searchText:string;
+  searchName(){
+    this.myObj=this.copyObj.filter((user)=>{
+      this.searchText=(this.searchText).replace(/\s/g,'');
+      if(this.searchText!='')
+        return (user.name.indexOf(this.searchText)!=-1)||((user.age).toString().indexOf(this.searchText)!=-1)||
+        user.car.indexOf(this.searchText)!=-1
+        return user.name;
+    })
+  }
   myObj = [{ "name":"John", "age":30, "car":"naveen" },{ "name":"gupta", "age":60, "car":"rolls royce" }];
+  copyObj= this.myObj;
 }
 
